36d2da2a2fa95975eb2fdbe439a527a862719bfa
Stanhuang08 <stanhuang08@gmail.com>
2021-03-18 01:52:43 +0800

    程式架構完成 excel輸出完成 自動更新時間待完成 自動排程待完成 程式中斷資料庫補資料待完成

--- CHT_IOT_HTTPRequest_GET (1).py
+++ CHT_IOT_HTTPRequest_GET (1).py
@@ -16,7 +16,8 @@ from io import StringIO
 
 def time_change(a):
     t2 = time.strptime(a, "%Y-%m-%d %H:%M:%S.%f") 
-    otherStyleTime = time.strftime("%Y/%m/%d %H:%M:%S", t2)
+    #otherStyleTime = time.strftime("%Y/%m/%d %H:%M:%S", t2)
+    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", t2)
     return otherStyleTime
 
 def get_battery_level(voltage_now, max_capactiy, min_capacity):
@@ -68,12 +69,12 @@ def check_control_value(typename, realtime_value, prewarning_value, alert_value,
         else:
             return '-'    
             
-def check_angle_value(realtime_value, prewarning_value, alert_value, last1, last2, last3,applicable):
+def check_angle_value(realtime_value, prewarning_value, alert_value, applicable):
     if applicable == 'Y':
         warning_check_message = '正常'
-        if float(realtime_value) >= float(prewarning_value) and float(last1) >= float(prewarning_value) and float(last2) >= float(prewarning_value) and float(last3) >= float(prewarning_value):
+        if float(realtime_value) >= float(prewarning_value):
             warning_check_message = '已達預警值'
-        if float(realtime_value) >= float(alert_value) and float(last1) >= float(alert_value) and float(last2) >= float(alert_value) and float(last3) >= float(alert_value): 
+        if float(realtime_value) >= float(alert_value): 
             warning_check_message = '已達警戒值'
         return warning_check_message
     else:
@@ -101,7 +102,7 @@ def get_data(a):
         my_headers = {'CK': CHANNEL_LIST['Read API Keys'][a]}
         channelid = CHANNEL_LIST['IOT_CHANNEL_ID'][a]
 
-        r = requests.get('https://iot.cht.com.tw/iot/v1/device/'+channelid+'/sensor/'+str(i)+'/rawdata?start=2021-03-14T16%3A00%3A00Z&end=2021-03-16T16%3A12%3A00Z&utcOffset=8', headers = my_headers)
+        r = requests.get('https://iot.cht.com.tw/iot/v1/device/'+channelid+'/sensor/'+str(i)+'/rawdata?start=2021-03-16T16%3A00%3A00Z&end=2021-03-18T16%3A12%3A00Z&utcOffset=8', headers = my_headers)
 
         #print(r.status_code)
 
@@ -145,9 +146,9 @@ df0 = get_data(0)
 df1 = get_data(1)
 df2 = get_data(2)
 df3 = get_data(3)
-print(df1)
-print(df2)
-print(df3)
+#print(df1)
+#print(df2)
+#print(df3)
 dbname = Path(SYS_path + '/Database.mdb')
 
 def insert_db(a, df):
@@ -178,9 +179,62 @@ def insert_db(a, df):
             cnxn.commit()
     print('ID',a,'數據寫入完成')
 
-insert_db(1, df1)
-insert_db(2, df2)
-insert_db(3, df3)
+def sort_out_new_data(a, b ,c ,d):
+    latest_result0 = a.tail(1)      
+    latest_result1 = b.tail(1)     
+    latest_result2 = c.tail(1)      
+    latest_result3 = d.tail(1)
+    aa = latest_result0.append(latest_result1)
+    aa = aa.append(latest_result2)
+    aa = aa.append(latest_result3)
+    aa = aa.reset_index(drop=True)
+    return aa
+
+SYS_var = pd.read_excel(SYS_path + '/Setting.xlsx', engine = 'openpyxl')
+Sync_Freq = SYS_var['Sync_Freq(min)'][0]
+Sync_Abnormal_Time = int(SYS_var['異常時間準則(小時)'][0])
+
+
+df_STATUS_TABLE = pd.DataFrame()
+df_STATUS_TABLE = sort_out_new_data(df0, df1, df2 ,df3)
+df_STATUS_TABLE.columns = ['deviceID', 'created_at', 'field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7', 'field8', 'localtime']
+
+run_status = []
+for i in range(0,4):    
+    now_Sync = datetime.datetime.now()  
+    data_lag = now_Sync - datetime.datetime.strptime(str(df_STATUS_TABLE['created_at'][i]), '%Y-%m-%d %H:%M:%S.%f')
+    data_lag_day = data_lag.days*24
+    data_lag_second = data_lag.seconds/3600        
+    data_lag = data_lag_day + data_lag_second
+
+    if data_lag < Sync_Abnormal_Time or now_Sync <=  datetime.datetime.strptime(str(df_STATUS_TABLE['created_at'][i]), '%Y-%m-%d %H:%M:%S.%f'):
+        run_status.append('正常')
+    else:
+        run_status.append('異常')
+df_STATUS_TABLE['儀器狀態'] = run_status
+
+df_STATUS_TABLE = pd.concat([CHANNEL_LIST, df_STATUS_TABLE], axis = 1)
+df_STATUS_TABLE = df_STATUS_TABLE.replace(to_replace='end', value='0', regex=True)
+
+
+
+df_STATUS_TABLE["電池百分比"] = df_STATUS_TABLE.apply(lambda row: get_battery_level(float(row['field2']), float(row['電池電壓(最大)']), float(row['電池電壓(最小)'])),axis=1)
+df_STATUS_TABLE["水位深度"] = df_STATUS_TABLE.apply(lambda row: water_level(float(row['高程']),float(row['field8']),float(row['field3']),str(row['模組'])),axis=1)
+df_STATUS_TABLE["水位高程"] = df_STATUS_TABLE.apply(lambda row: t_water_level(float(row['field8']),float(row['field7']),str(row['模組'])),axis=1)
+df_STATUS_TABLE["傾角1"] = df_STATUS_TABLE.apply(lambda row:get_true_angle(row['field3'], row['傾角1初始值'], str(row['傾角1判定'])) ,axis=1)
+df_STATUS_TABLE["傾角2"] = df_STATUS_TABLE.apply(lambda row:get_true_angle(row['field4'], row['傾角2初始值'], str(row['傾角2判定'])) ,axis=1)
+
+df_STATUS_TABLE["水位管理值判定"] = df_STATUS_TABLE.apply(lambda row: check_control_value('水位',float(row['水位深度']), float(row['預警值(水位)']), float(row['警戒值(水位)']), str(row['水位判定'])),axis=1)
+df_STATUS_TABLE["傾角1管理值判定"] = df_STATUS_TABLE.apply(lambda row: check_angle_value(row['傾角1'], row['預警值(傾角)'], row['警戒值(傾角)'], str(row['傾角1判定'])),axis=1)
+df_STATUS_TABLE["傾角2管理值判定"] = df_STATUS_TABLE.apply(lambda row: check_angle_value(row['傾角2'], row['預警值(傾角)'], row['警戒值(傾角)'],  str(row['傾角2判定'])),axis=1)
+print(df_STATUS_TABLE)
+STATUS_TABLE_Path = SYS_path + '/STATUS_TABLE_OUTPUT/' + 'Monitoring_Status.xlsx'   
+writer = pd.ExcelWriter(STATUS_TABLE_Path)
+df_STATUS_TABLE.to_excel(writer,'Sheet1')
+writer.save()  
+
+
+
 
 
 
